4ec98e5238c9cc5270c816aa0bf6ee1e34dcda6d
FernandaSoares22 <161779706+FernandaSoares22@users.noreply.github.com>
2025-06-04 15:06:50 -0300

    Create exercicio10.py

---
+++ arvore/busca/exercicio10.py
@@ -0,0 +1,3 @@
+# 10. Escreva uma função que inverta os filhos de todos os nós de uma árvore binária (i.e. todos os filhos da
+# direita passam a ser filhos da esquerda e vice-versa). Tal função deve ter o protótipo: def inverte(a)
+# Qual é o resultado esperado da função ao executar sobre a árvore abaixo?
